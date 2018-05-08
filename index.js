fd7b000cd4fc1391cfc96b0aea8e5ebd3436d353
Alexandru Olaru <alxolr@gmail.com>
2018-05-08 11:12:49 +0300

    Added list and inquirer for the application

--- index.js
+++ index.js
@@ -1,34 +1,51 @@
+#!/usr/bin/node
+
 'use strict';
 
 const mongoose = require('mongoose');
 const program = require('commander');
+const dotenv = require('dotenv');
 const deepinity = require('./src/deepinity');
+require('colors');
+
+dotenv.load();
 
 mongoose.Promise = Promise;
 const user = process.env.MONGO_USER;
 const pass = process.env.MONGO_PASSWORD;
-const uri = 'mongodb+srv://alxolr-apps-2j4du.mongodb.net/deepinity';
+const uri = `mongodb+srv://${user}:${pass}@alxolr-apps-2j4du.mongodb.net/deepinity`;
 
-mongoose.connect(uri, { user, pass })
+mongoose.connect(uri)
   .then(startProgram)
   .catch(handleError);
 
 function startProgram() {
-  console.log('Application has connected');
-
-  program
-    .version('0.0.1')
-    .command('log <duration> [comment]')
-    .description('add a meditation log')
-    .action(deepinity.log);
-
   program.parse(process.argv);
+
   if (!program.args.length) program.help();
 }
 
 function handleError(err) {
   console.error(err);
 }
+
+program
+  .version('0.0.1');
+
+program
+  .command('log <duration> [comment]')
+  .description('add a meditation log')
+  .action(deepinity.log);
+
+program
+  .command('list [limit] [skip]')
+  .description('list previous meditations')
+  .action(deepinity.list);
+
+program
+  .command('remove <entry>')
+  .description('remove an entry by id')
+  .action(deepinity.remove);
 
 process.on('unhandledRejection', console.error);
 process.on('uncaughtException', console.error);
--- src/deepinity.js
+++ src/deepinity.js
@@ -1,5 +1,69 @@
 'use strict';
 
+const Sit = require('./models/sit.model');
+const mongoose = require('mongoose');
+const { prompt } = require('inquirer');
+
+const { ObjectId } = mongoose.Types;
+
 module.exports = {
+  log,
+  list,
+  remove,
 };
 
+function log(duration, comment) {
+  const sit = new Sit({ duration, comment });
+
+  return sit
+    .save()
+    .then(() => {
+      console.log('meditation was logged'.green);
+      mongoose.disconnect();
+    })
+    .catch(handleError);
+}
+
+function list(limit = 10, skip = 0) {
+  return Sit.find()
+    .skip(+skip)
+    .limit(+limit)
+    .sort({ createdAt: -1 })
+    .lean()
+    .then((docs) => {
+      const choices = docs.map(e => ({
+        name: `${e.createdAt} - ${e._id.toHexString().magenta} - ${(`${e.duration} minutes`).green}`,
+        value: e._id.toHexString(),
+      }));
+
+      const questions = {
+        type: 'list',
+        message: 'Select a meditation for details',
+        name: 'sits',
+        choices,
+      };
+
+      return prompt(questions);
+    })
+    .then(answer => Sit.findById(answer.sits))
+    .then((sit) => {
+      console.log(sit);
+      mongoose.disconnect();
+    })
+    .catch(handleError);
+}
+
+function remove(entry) {
+  return Sit.deleteOne({ _id: new ObjectId(entry) })
+    .then(() => {
+      console.log('%s entry was deleted', entry);
+      mongoose.disconnect();
+    })
+    .catch(handleError);
+}
+
+
+function handleError(err) {
+  console.error(err);
+  mongoose.disconnect();
+}
